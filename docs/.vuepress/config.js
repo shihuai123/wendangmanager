f3cc7605d842c6c3c9fca7cb141b61c4b39080e4
Your Name <you@example.com>
2019-05-06 10:22:29 +0800

    更新文档管理

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: '郑州幻视科技有限公司软件设计文档',
+    description: '郑州幻视科技有限公司',
+    base:'/',
+    head: [
+        ['link', { rel: 'icon', href: './icon.ico' }]
+    ],
+    themeConfig: {
+        sidebar: [
+            '/',
+            '/two',
+            '/three',
+            '/four'
+        ],
+        lastUpdated: 'Last Updated'
+    },
+
+}
